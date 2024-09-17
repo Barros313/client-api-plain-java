85d42ff278efd6d46d6f6ffc738b0dc0f620e3ba
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-09-17 11:47:21 -0300

    Added script to create client table
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- src/main/java/com/barros/CreateDB.java
+++ src/main/java/com/barros/CreateDB.java
@@ -1,2 +1,28 @@
-package com.barros;public class CreateDB {
+package com.barros;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateDB {
+    static Connection connection = DBConnection.getConnection();
+
+    public static void main(String[] args) {
+        String query = "CREATE TABLE IF NOT EXISTS clients (" +
+                "id SERIAL PRIMARY KEY, " +
+                "name VARCHAR(60) NOT NULL, " +
+                "age INT NOT NULL);";
+
+        Statement statement = null;
+
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+
+            System.out.println("Table created");
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e.getMessage());
+        }
+    }
 }
