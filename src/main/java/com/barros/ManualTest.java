4b0c60203d1bcd67aec8e9c189600a9d735113f7
Barros313 <gbarros1337@gmail.com>
2024-09-17 20:03:22 -0300

    Finished test database logic
    Signed-off-by: Barros313 <gbarros1337@gmail.com>

--- src/main/java/com/barros/ManualTest.java
+++ src/main/java/com/barros/ManualTest.java
@@ -5,11 +5,23 @@ import com.barros.view.ClientOperations;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManualTest {
     public static void main(String[] args) throws SQLException {
         ClientOperations exec = new ClientOperations();
 
-        System.out.println(exec.getClient(1));
+        List<Client> clients = new ArrayList<>();
+
+        clients.add(new Client("Mylene Cavalcanti", 22));
+        clients.add(new Client("Davi Barros", 20));
+        clients.add(new Client("Hugo Vasconcelos", 20));
+
+        for (Client client : clients) {
+            exec.add(client);
+        }
+
+        System.out.println(exec.getAllClients());
     }
 }
