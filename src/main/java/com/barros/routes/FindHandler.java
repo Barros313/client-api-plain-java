1f2ee246f3cf2f095c25f31c862d07c5bd546fab
Barros313 <gbarros1337@gmail.com>
2024-09-17 20:04:17 -0300

    Finished get implementation
    Signed-off-by: Barros313 <gbarros1337@gmail.com>

--- src/main/java/com/barros/routes/FindHandler.java
+++ src/main/java/com/barros/routes/FindHandler.java
@@ -1,2 +1,65 @@
-package com.barros.routes;public class FindHandler {
+package com.barros.routes;
+
+import com.barros.models.Client;
+import com.barros.view.ClientOperations;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.sql.SQLException;
+
+public class FindHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String requestMethod = exchange.getRequestMethod();
+
+        URI requestURI = exchange.getRequestURI();
+        String path = requestURI.getPath();
+
+        String[] pathParts = path.split("/");
+
+        System.out.println(pathParts[2]);
+
+        OutputStream responseBody = exchange.getResponseBody();
+        long clientId = 0;
+        try {
+            clientId = Integer.parseInt(pathParts[2]);
+            System.out.println(clientId);
+        } catch (NumberFormatException exp) {
+            responseBody.write("Bad request".getBytes());
+            System.err.println("Bad request: " + exp.getMessage());
+            return;
+        }
+
+        ClientOperations op = new ClientOperations();
+        Client client;
+
+        try {
+            client = op.getClient((int) clientId);
+        } catch (SQLException e) {
+            responseBody.write("Error".getBytes());
+            System.err.println("Bad request: " + e.getMessage());
+            return;
+        }
+
+        if (client == null) {
+            responseBody.write("Client not found".getBytes());
+            System.err.println("Client not found");
+            return;
+        }
+
+        exchange.getResponseHeaders().add("Content-Type", "application/json");
+        ObjectMapper mapper = new ObjectMapper();
+
+        String response = mapper.writeValueAsString(client);
+        System.out.println(response);
+
+        responseBody.write(response.getBytes());
+        responseBody.flush();
+        responseBody.close();
+    }
 }
