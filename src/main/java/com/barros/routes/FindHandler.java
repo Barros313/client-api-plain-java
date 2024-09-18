c5d836e2fe5ae6c5407d5a78b499746655b658bf
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-09-18 11:27:37 -0300

    Finished implementing find handler with exception handles
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- src/main/java/com/barros/routes/FindHandler.java
+++ src/main/java/com/barros/routes/FindHandler.java
@@ -15,50 +15,97 @@ import java.sql.SQLException;
 public class FindHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String requestMethod = exchange.getRequestMethod();
 
+        // Get request path
         URI requestURI = exchange.getRequestURI();
         String path = requestURI.getPath();
 
+        // Split path parts
         String[] pathParts = path.split("/");
 
-        System.out.println(pathParts[2]);
-
+        // Set output body
         OutputStream responseBody = exchange.getResponseBody();
-        long clientId = 0;
+
+        // Get client Id through path part
+        int clientId = 0;
         try {
+            // Get integer from url path
             clientId = Integer.parseInt(pathParts[2]);
-            System.out.println(clientId);
         } catch (NumberFormatException exp) {
-            responseBody.write("Bad request".getBytes());
-            System.err.println("Bad request: " + exp.getMessage());
+            // Set bad request message
+            String nfeMessage = "Bad request";
+
+            // Set bad request status code and message length
+            exchange.getResponseHeaders().add("Content-Type", "text/plain");
+            exchange.sendResponseHeaders(400, nfeMessage.length());
+
+            // Write to body
+            responseBody.write(nfeMessage.getBytes());
+
+            // Print exception to terminal
+            System.err.println(nfeMessage + "\n" + exp.getMessage());
+
+            // End program
             return;
         }
 
+        // Get client operations
         ClientOperations op = new ClientOperations();
         Client client;
 
+        // Get client
         try {
-            client = op.getClient((int) clientId);
+            // Get client
+            client = op.getClient(clientId);
         } catch (SQLException e) {
-            responseBody.write("Error".getBytes());
-            System.err.println("Bad request: " + e.getMessage());
+            // Define SQL error message
+            String sqlError = "SQL Error";
+
+            // Set status code and response length
+            exchange.getResponseHeaders().add("Content-Type", "text/plain");
+            exchange.sendResponseHeaders(500, sqlError.length());
+
+            // Write error message to body
+            responseBody.write(sqlError.getBytes());
+
+            // Print exception message to terminal
+            System.err.println(sqlError + "\n" + e.getMessage());
+
+            // End program
             return;
         }
 
+        // NULL check
         if (client == null) {
-            responseBody.write("Client not found".getBytes());
-            System.err.println("Client not found");
+            // Define null client message
+            String nullClient = "Client not found";
+
+            // Set client not found status code and message length
+            exchange.getResponseHeaders().add("Content-Type", "text/plain");
+            exchange.sendResponseHeaders(404, nullClient.length());
+
+            // Write error message to body
+            responseBody.write(nullClient.getBytes());
+
+            // Print exception message
+            System.err.println(nullClient + "\n" + nullClient);
+
+            // End program
             return;
         }
 
-        exchange.getResponseHeaders().add("Content-Type", "application/json");
+        // Convert to JSON string
         ObjectMapper mapper = new ObjectMapper();
-
         String response = mapper.writeValueAsString(client);
-        System.out.println(response);
 
+        // Set status code and response length
+        exchange.getResponseHeaders().add("Content-Type", "application/json");
+        exchange.sendResponseHeaders(200, response.length());
+
+        // Write to body
         responseBody.write(response.getBytes());
+
+        // Finish
         responseBody.flush();
         responseBody.close();
     }
