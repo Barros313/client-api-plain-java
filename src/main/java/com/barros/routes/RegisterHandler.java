d83d1df7635131d056bf0061f38e3e08c809ae11
Barros313 <gbarros1337@gmail.com>
2024-09-18 20:55:59 -0300

    Implemented register client route
    Signed-off-by: Barros313 <gbarros1337@gmail.com>

--- src/main/java/com/barros/routes/RegisterHandler.java
+++ src/main/java/com/barros/routes/RegisterHandler.java
@@ -1,2 +1,115 @@
-package com.barros.routes;public class RegisterHandler {
+package com.barros.routes;
+
+import com.barros.models.Client;
+import com.barros.view.ClientOperations;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class RegisterHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        InputStream inputStream = exchange.getRequestBody();
+        OutputStream responseBody = exchange.getResponseBody();
+        ObjectMapper mapper = new ObjectMapper();
+        Scanner scanner = new Scanner(inputStream);
+        ClientOperations op = new ClientOperations();
+
+        // Get body JSON input
+        String bodyInput;
+        try {
+            bodyInput = scanner.useDelimiter("\\A").next();
+        } catch (IllegalStateException ise) {
+            // Print exception to console
+            System.err.println(ise.getMessage());
+
+            // Print error message
+            String errorMessage = "Error reading data";
+            exchange.getResponseHeaders().add("Content-Type", "plain/text");
+            exchange.sendResponseHeaders(500, errorMessage.length());
+            responseBody.write(errorMessage.getBytes());
+
+            // End
+            responseBody.flush();
+            responseBody.close();
+            return;
+        } catch (NoSuchElementException nsee) {
+            // Print exception to console
+            System.err.println(nsee.getMessage());
+
+            // Print error message
+            String errorMessage = "Empty data";
+            exchange.getResponseHeaders().add("Content-Type", "plain/text");
+            exchange.sendResponseHeaders(400, errorMessage.length());
+            responseBody.write(errorMessage.getBytes());
+
+            // End
+            responseBody.flush();
+            responseBody.close();
+            return;
+
+        }
+
+        // Map to client object
+        Client newClient;
+        try {
+            newClient = mapper.readValue(bodyInput, Client.class);
+        } catch (JsonProcessingException e) {
+            // Print exception to console
+            System.err.println(e.getMessage());
+
+            // Print error message
+            String errorMessage = "Not a valid JSON object";
+            exchange.getResponseHeaders().add("Content-Type", "plain/text");
+            exchange.sendResponseHeaders(400, errorMessage.length());
+            responseBody.write(errorMessage.getBytes());
+
+            // End
+            responseBody.flush();
+            responseBody.close();
+            return;
+        }
+
+        int newClientId = 0;
+        try {
+            newClientId = op.add(newClient);
+        } catch (SQLException exp) {
+            // Print exception to console
+            System.err.println(exp.getMessage());
+
+            // Print error message
+            String errorMessage = "Failed to add client";
+            exchange.getResponseHeaders().add("Content-Type", "plain/text");
+            exchange.sendResponseHeaders(502, errorMessage.length());
+            responseBody.write(errorMessage.getBytes());
+
+            // End
+            responseBody.flush();
+            responseBody.close();
+            return;
+        }
+
+        // Print success message to console
+        System.out.println("Cliend id: " + newClientId + " successfully added");
+
+        // Print success message to response body
+        String success = "Successfully added client";
+        exchange.getResponseHeaders().add("Content-Type", "text/plain");
+        exchange.sendResponseHeaders(200, success.length());
+        responseBody.write(success.getBytes());
+
+        // End
+        responseBody.flush();
+        responseBody.close();
+        return;
+    }
 }
