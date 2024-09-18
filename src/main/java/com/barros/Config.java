2859548249a2b7d518b82eb6fb4d33f6ebcbfc8c
Barros313 <gbarros1337@gmail.com>
2024-09-18 19:22:29 -0300

    Added configuration class
    Signed-off-by: Barros313 <gbarros1337@gmail.com>

--- src/main/java/com/barros/Config.java
+++ src/main/java/com/barros/Config.java
@@ -1,2 +1,26 @@
-package com.barros;public class Config {
+package com.barros;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+    private final Properties properties = new Properties();
+
+    public Config() {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.err.println("No config.properties file");
+                return;
+            }
+
+            properties.load(input);
+        } catch (IOException | IllegalArgumentException | NullPointerException exp) {
+            System.err.println(exp.getMessage());
+        }
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
